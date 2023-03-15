d738742c21fa4ac0a2c18d6338343854249b0109
Олег Спас <spas.oleh@gmail.com>
2023-03-15 20:56:53 +0100

    Code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         String text =
                 "National Forestry University of Ukraine, Institute of Woodworking,\n" +
                         "Computer Technology and Design, IST-21, 126 Information systems and technologies"
-
                 ;
         // Task 1
         char[] characterArray = {'N', 'a', 't', 'i', 'o', 'n', 'a', 'l', ' ', 'F', 'o', 'r', 'e', 's', 't', 'r', 'y', ' ',
@@ -15,57 +14,42 @@ public class Main {
         // Task 2
         String string1 = "National Forestry University of Ukraine, Institute of Woodworking,Computer Technology and Design, IST-21, 126 Information systems and technologies ";
         System.out.println("Task 2: " + string1.substring(0, string1.lastIndexOf(",") + 1));
-
         // Task 3
         StringBuilder text2 = new StringBuilder();
         text2.append(string1);
         text2.reverse();
         System.out.println("Task 3: " + text2);
-
         //Task 4
         String string3 = string1.toUpperCase();
         System.out.println("Task 4: " + string3);
-
         System.out.println("Task 4: " +string1.equals(string3));
-
         // Task 5
         System.out.println("Task 5: " + string1.split(", ")[2]);
-
         //Task 6
         String string4 = string1 + string3;
         System.out.println("Task 6: " + string4);
-
         // Task 7
         System.out.println("Task 7: Індекс першої букви 'N' :" + string1.indexOf("N"));
         System.out.println("Task 7: Індекс останньої букви 'r' :" + string1.lastIndexOf("r"));
-
         // Task 8
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append ("Spas Oleh Ihorovych 12.05.2004");
         System.out.println("Task 8: " + stringBuffer);
-
         // Task 9
         System.out.println("Task 9: " + stringBuffer.substring( 0, stringBuffer.lastIndexOf("h") + 1));
-
         // Task 10
         String placeOfBirth = ", Lviv, Ukraine";
         System.out.println("Task 10: " + stringBuffer.append(placeOfBirth));
-
         // Task 11
         stringBuffer.insert(31, " Male,");
         System.out.println("Task 11: " + stringBuffer);
-
         // Task 12
-
         System.out.println("Task 12: characters length: " + stringBuffer.length());
         System.out.println("Task 12: bytes length: " +  stringBuffer.toString().getBytes().length);
-
         // Task 13
         System.out.println("Task 13: " + stringBuffer.delete(stringBuffer.length() - 32, stringBuffer.length()));
-
         // Task 14
         stringBuffer.reverse();
         System.out.println("Task 14: " + stringBuffer);
-
     }
 }
